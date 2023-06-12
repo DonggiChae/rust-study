8e225bc8876108f49410c8f9bf56005b057c7cff
donggi chae <69336797+DonggiChae@users.noreply.github.com>
2023-06-12 16:35:53 +0900

    week4

---
+++ src/week4/mytest_struct/target/debug/deps/mytest_struct-0fa1845ea2cc2ffd.d
@@ -0,0 +1,5 @@
+/Users/donggichae/Documents/BlockChain/Rust/rust-study/src/week4/mytest_struct/target/debug/deps/mytest_struct-0fa1845ea2cc2ffd: src/lib.rs
+
+/Users/donggichae/Documents/BlockChain/Rust/rust-study/src/week4/mytest_struct/target/debug/deps/mytest_struct-0fa1845ea2cc2ffd.d: src/lib.rs
+
+src/lib.rs:
---
+++ src/week4/mytest_struct/target/debug/deps/mytest_struct-5b8531d659c3bdb2.d
@@ -0,0 +1,7 @@
+/Users/donggichae/Documents/BlockChain/Rust/rust-study/src/week4/mytest_struct/target/debug/deps/mytest_struct-5b8531d659c3bdb2.rmeta: src/lib.rs
+
+/Users/donggichae/Documents/BlockChain/Rust/rust-study/src/week4/mytest_struct/target/debug/deps/libmytest_struct-5b8531d659c3bdb2.rlib: src/lib.rs
+
+/Users/donggichae/Documents/BlockChain/Rust/rust-study/src/week4/mytest_struct/target/debug/deps/mytest_struct-5b8531d659c3bdb2.d: src/lib.rs
+
+src/lib.rs:
